7a25b6b2b468f97e82e5ed6f27b18f6f7023493a
veraabad <abad_vera@me.com>
2015-04-21 16:39:43 -0700

    Initial Commit

---
+++ ContributionCenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContributionCenter
+//
+//  Created by Abad Vera on 4/21/15.
+//  Copyright (c) 2015 Abad Vera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
